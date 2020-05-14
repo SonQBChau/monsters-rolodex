e9493921ba33808d0c43bac25e8b588a0a8cf702
SonChau <soncqb@gmail.com>
2020-05-14 21:50:59 +0700

    30. card component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
-import {CardList} from './components/card-list/card-list.component';
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
-      monsters: []
-    }
+      monsters: [],
+    };
   }
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({monsters: users}))
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
-
-
-
 
   render() {
-  return (
-    <div className="App">
-    <CardList name="SonChau">
-      {
-        this.state.monsters.map(monster => (
-          <h1 key={monster.id}> {monster.name} </h1>
-      ))}
-    </CardList>
-
-
-    </div>
-  );
+    return (
+      <div className="App">
+        <CardList monsters={this.state.monsters}></CardList>
+      </div>
+    );
   }
 }
 
