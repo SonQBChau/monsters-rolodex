1839b157a190051d4e5821d76b6288f3d7d90513
SonChau <soncqb@gmail.com>
2020-05-18 09:44:22 +0700

    Updates

---
+++ precache-manifest.b3b828693446b6432838b4e9f2f38c65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90bb74d56761af2a1309cc18cb040fe7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4a06fede15df92785a44",
+    "url": "/monsters-rolodex/static/css/main.d578ea19.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a06fede15df92785a44",
+    "url": "/monsters-rolodex/static/js/main.c99d3fdc.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
